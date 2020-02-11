9ab827a0817569e2415210af2eb52d8ca233735a
joel8716 <34272541+joel8716@users.noreply.github.com>
2020-02-11 19:53:08 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jquery 30 aula</title>
+	<style type="text/css">
+		.div {
+			width: 150px;
+			height: 150px; 
+			position: absolute;
+			background-color: #ddd;
+		}
+		
+	</style>
+</head>
+	<body>
+
+		<form method="POST" id="form">
+			
+			Numero 1<br>
+			<input type="text" name="x"><br><br>
+
+			Numero 2<br>
+			<input type="text" name="y"><br><br>
+
+			<input type="submit" name="Entrar">
+
+		</form>
+
+		<div class="div"><br><br><br><br><br><br><br><br><br>
+		<button>Ação</button>
+
+		<script type="text/javascript" src="jquery-3.4.1.min.js"></script>
+		<script type="text/javascript" src="script.js"></script>
+	</body>
+</html>
